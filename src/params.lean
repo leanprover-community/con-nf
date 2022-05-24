606fc003baf787ed5d838d39cbec190bf152691b
thirdsgames <thirdsgames2018@gmail.com>
2022-05-25 00:21:34 +0100

    Document model parameters
    Signed-off-by: thirdsgames <thirdsgames2018@gmail.com>

--- src/params.lean
+++ src/params.lean
@@ -1,8 +1,12 @@
 import set_theory.cardinal.cofinality
 
 /-!
-# Parameters of the construction -/
+# Parameters of the construction
+This file is based on sections 3.1 and 3.2 of the paper. -/
 
+/-- We do not assume that our definitions, instances, lemmas and theorems are 'computable';
+that is, can be provably evaluated in finite time by a computer.
+For our purposes, restricting to only computable functions is unnecessary. -/
 noncomputable theory
 
 open cardinal
@@ -12,7 +16,21 @@ universe u
 
 namespace con_nf
 
-/-- The parameters of the constructions. We collect them all in one class for simplicity. -/
+/--
+The parameters of the constructions. We collect them all in one class for simplicity.
+Note that the ordinal `λ` in the paper is instead referred to here as `Λ`, since the symbol `λ` is
+used for lambda abstractions.
+
+Ordinals and cardinals are represented here as arbitrary types (not sets) with certain properties.
+For instance, `Λ` is an arbitrary type that has an ordering `Λr`, which is assumed to be a
+well-ordering (the `Λwf` term is a proof of this fact).
+
+The prefix `#` denotes the cardinality of a type.
+
+Where possible, we use `<` and `≤` instead of `>` and `≥`. Human readers can easily convert between
+the two, but it is not as immediately transparent for Lean. For simplicity, we keep with the
+convention of using only `<` and `≤`.
+-/
 class params :=
 (Λ : Type u) (Λr : Λ → Λ → Prop) [Λwf : is_well_order Λ Λr]
 (hΛ : (ordinal.type Λr).is_limit)
@@ -28,9 +46,13 @@ open params
 
 variables [params.{u}]
 
+/-- To allow Lean's type checker to see that the ordering `Λr` is a well-ordering without having to
+explicitly write `Λwf` everywhere, we declare it as an instance.
+We can then deduce from the well-ordering that `Λ` is linearly ordered. -/
 instance : is_well_order Λ Λr := Λwf
 instance : linear_order Λ := linear_order_of_STO' Λr
 
+-- Since `μ` has cofinality `≥ κ`, the cardinality of `κ` must be at most that of `μ`.
 lemma κ_le_μ : #κ ≤ #μ := κ_le_μ_cof.trans $ ordinal.cof_ord_le _
 
 /-- The base type of the construction, `τ₋₁` in the document. Instead of declaring it as an
@@ -38,6 +60,8 @@ arbitrary type of cardinality `μ` and partitioning it in parts of cardinality `
 define it as `μ × κ`, which has the correct cardinality and comes with an obvious partition. -/
 def base_type : Type* := μ × κ
 
+/-- The cardinality of `τ₋₁` is the cardinality of `μ`.
+We will prove that all types constructed in our model have cardinality equal to `μ`. -/
 @[simp] lemma mk_base_type : #base_type = #μ :=
 by simp_rw [base_type, mk_prod, lift_id,
   mul_eq_left (κ_regular.omega_le.trans κ_le_μ) κ_le_μ κ_regular.pos.ne']
