433d4e296cc9c7c02c649e5c474e2b1f901a9e26
thirdsgames <thirdsgames2018@gmail.com>
2022-06-14 21:37:51 +0100

    Membership for pretangles
    Signed-off-by: thirdsgames <thirdsgames2018@gmail.com>

--- src/pretangle.lean
+++ src/pretangle.lean
@@ -27,4 +27,26 @@ def pretangle : Λ → Type u
     ({β : type_index | β < α})
 using_well_founded { dec_tac := `[assumption] }
 
+namespace pretangle
+
+def members {α : Λ} (a : pretangle α) : Π (β < α), set (pretangle β) :=
+by { unfold pretangle at a, exact a.1 }
+
+-- TODO: Why is there an `option` in the set of base members?
+def base_members {α : Λ} (a : pretangle α) : option (set base_type) :=
+by { unfold pretangle at a, exact a.2.1 }
+
+def preferred_extension {α : Λ} (a : pretangle α) : {β : type_index | β < α} :=
+by { unfold pretangle at a, exact a.2.2 }
+
+instance has_mem {α β : Λ} (h : β < α) :
+has_mem (pretangle β) (pretangle α) :=
+⟨λ b a, b ∈ a.members β h⟩
+
+instance has_mem_base_type {α : Λ} :
+has_mem base_type (pretangle α) :=
+⟨λ b a, a.base_members.elim false (λ s, b ∈ s)⟩
+
+end pretangle
+
 end con_nf
