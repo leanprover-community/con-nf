fcaee35534d066d9b2dd1e6da1a1a582c0e678f5
thirdsgames <thirdsgames2018@gmail.com>
2022-06-16 17:59:51 +0100

    Derivatives
    Signed-off-by: thirdsgames <thirdsgames2018@gmail.com>

--- src/code.lean
+++ src/code.lean
@@ -3,6 +3,8 @@ import group_theory.perm.basic
 import litter
 import params
 
+noncomputable theory
+
 universe u
 
 namespace con_nf
@@ -54,6 +56,30 @@ def lower_code_perm (π : struct_perm β) (γ < β) : struct_perm γ :=
 by { unfold struct_perm at π, exact π.2 γ ‹_›}
 
 -- There is no permutation on β-codes since the structural permutations do not act on tangles.
+
+/-- We construct the ordering on extended type indices such that when we remove the lowest arrow,
+the resulting path is considered smaller. -/
+lemma drop_min_lt {β : Λ} {γ δ : type_index}
+(a : quiver.path (β : type_index) γ) (b : γ ⟶ δ) (c : δ ⟶ ⊥) :
+extended_index.lt (a.cons (lt_trans c b) : extended_index β) ((a.cons b).cons c : extended_index β) :=
+begin
+  sorry
+end
+
+noncomputable def derivative_core : Π (A : extended_index β), struct_perm β → struct_perm A.min
+| (quiver.path.cons quiver.path.nil _) := id
+| (@quiver.path.cons _ _ _ p _ (@quiver.path.cons _ _ _ q _ a b) c) :=
+  λ π, begin
+    refine (derivative_core (quiver.path.cons a (lt_trans c b)) π).lower_code_perm _ _,
+    induction p, { unfold quiver.hom at c, simp at c, contradiction },
+    induction q, { unfold quiver.hom at b, simp at b, contradiction },
+    unfold quiver.hom at b, simp at b, exact b
+  end
+using_well_founded { dec_tac := `[exact drop_min_lt a b c] }
+
+/-- The derivative of a structural permutation with respect to a given extended type index. -/
+def derivative (π : struct_perm β) (A : extended_index β) : struct_perm A.min :=
+derivative_core A π
 
 end struct_perm
 
