352fcff4a16a1e53c06e78422b26584ae537c4a7
thirdsgames <thirdsgames2018@gmail.com>
2022-06-18 18:36:11 +0100

    Rename recursion motor to phase_1a
    Signed-off-by: thirdsgames <thirdsgames2018@gmail.com>

--- src/code.lean
+++ src/code.lean
@@ -12,21 +12,21 @@ variable [params.{u}]
 
 open params
 
-/-- The motor of the recursion. This contains all the information needed for the main recursion.
-Expect it to grow bigger with time. -/
-class recursion_motor (α : Λ) :=
+/-- The motor of the initial recursion. This contains all the information needed for phase 1a of the
+recursion. -/
+class phase_1a (α : Λ) :=
 (tangle : Π β < α, Type u)
 (to_tangle : Π β (h : β < α), litter ↪ tangle β h)
 (of_tangle : Π β (h : β < α), tangle β h ↪ μ)
 
-export recursion_motor (of_tangle to_tangle)
+export phase_1a (of_tangle to_tangle)
 
-variables (α : Λ) [recursion_motor.{u} α]
+variables (α : Λ) [phase_1a.{u} α]
 
 /-- The tangles already constructed at stage `α`. -/
 def tangle : Π β < (α : type_index), Type u
 | ⊥ h := atom
-| ((β : Λ) : type_index) h := recursion_motor.tangle β $ coe_lt_coe.1 h
+| ((β : Λ) : type_index) h := phase_1a.tangle β $ coe_lt_coe.1 h
 
 /-- A type-`β` code is a type index `γ < β` together with a set of tangles of type `γ`. -/
 structure code (β : Λ) (β_le_α : β ≤ α) :=
@@ -35,11 +35,11 @@ structure code (β : Λ) (β_le_α : β ≤ α) :=
 (elts : set (tangle α extension (extension_lt.trans_le $ coe_le_coe.mpr β_le_α)))
 
 /-- Suppose that the set of tangles embeds into the set of codes. -/
-class recursion_motor_embedding :=
+class phase_1a_embedding :=
 (tangle_embedding : Π (β < α), tangle _ β (coe_lt_coe.mpr ‹_›) ↪ code _ β (le_of_lt ‹_›))
 
-open recursion_motor_embedding
-variable [recursion_motor_embedding.{u} α]
+open phase_1a_embedding
+variable [phase_1a_embedding.{u} α]
 
 def code.is_tangle {β < α} (c : code α β (le_of_lt ‹_›)) : Prop :=
 ∃ t : tangle α β (coe_lt_coe.2 ‹_›), c = tangle_embedding β ‹_› t
