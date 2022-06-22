d9fd8a827994461e207cf21e95c67391c2e64ff5
amm299 <corp.matthews@protonmail.com>
2022-06-22 15:46:55 +0100

    Proved well-foundedness can be pulled back.

--- src/mathlib/well_founded.lean
+++ src/mathlib/well_founded.lean
@@ -15,6 +15,41 @@ instance with_bot_is_well_order (α : Type*)
 ⟨with_bot.well_founded_lt β.2⟩
 
 /-- Pullback well-foundedness. -/
+
+private def preimage_accessible {r : β → β → Prop} (f : α → β) (h : well_founded r) (y : β) :=
+∀ x : α, f x = y → acc (inv_image r f) x
+
 lemma well_founded.inv_image {r : β → β → Prop} (f : α → β) (h : well_founded r) :
   well_founded (inv_image r f) :=
-sorry -- might need `f` injective
+  begin
+      split,
+      have preimage_accessible_inductive : Π (x_2 : β), (Π (y : β), r y x_2 →
+          preimage_accessible f h y) → preimage_accessible f h x_2,
+      {
+        intros y hy x hx,
+        split,
+        intros x_2 hx_2,
+        unfold inv_image at hx_2,
+        specialize hy (f x_2),
+        have key : preimage_accessible f h (f x_2),
+        {
+          apply hy,
+          rw hx at hx_2,
+          exact hx_2
+        },
+        unfold preimage_accessible at key,
+        specialize key x_2,
+        apply key; refl,
+      },
+      have all_preimages_accessible : ∀ y : β, preimage_accessible f h y,
+      {
+          apply well_founded.fix h preimage_accessible_inductive
+      },
+      intro x,
+      have key :  ∀ x_2 : α, f x_2 = f x → acc (inv_image r f) x_2,
+      {
+        apply all_preimages_accessible
+      },
+      specialize key x,
+      apply key; refl,
+  end
