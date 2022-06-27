4f2830d1c5523814b991af51e2e407468a9ccfbd
thirdsgames <thirdsgames2018@gmail.com>
2022-06-27 23:24:54 +0100

    Implement allowable permutation group
    Signed-off-by: thirdsgames <thirdsgames2018@gmail.com>

--- src/allowable.lean
+++ src/allowable.lean
@@ -27,15 +27,38 @@ export phase_1b (allowable allowable_group to_structural allowable_action)
 
 variables [phase_1b.{u} α]
 
-instance allowable_group_pi : group (Π β (h : β < α), allowable β h) := sorry
+-- TODO(zeramorphic): When `pi.group` works with `Prop`, change this to just an invocation of `pi.group`.
+instance allowable_group_pi' {β : Λ} (hβ : β ≤ α) (γ : Λ) :
+group (∀ (h : γ < β), allowable γ (h.trans_le hβ)) := {
+  one := (λ h, (allowable_group γ (h.trans_le hβ)).one),
+  mul := (λ x y h, @group.mul _ (allowable_group γ (h.trans_le hβ)) (x h) (y h)),
+  one_mul := (λ x, funext (λ h, @group.one_mul _ (allowable_group γ (h.trans_le hβ)) (x h))),
+  mul_one := (λ x, funext (λ h, @group.mul_one _ (allowable_group γ (h.trans_le hβ)) (x h))),
+  mul_assoc := (λ x y z, funext (λ h, @group.mul_assoc _ (allowable_group γ (h.trans_le hβ)) (x h) (y h) (z h))),
+  inv := (λ x h, @group.inv _ (allowable_group γ (h.trans_le hβ)) (x h)),
+  mul_left_inv := (λ x, funext (λ h, @group.mul_left_inv _ (allowable_group γ (h.trans_le hβ)) (x h))),
+  div := (λ x y h, @group.div _ (allowable_group γ (h.trans_le hβ)) (x h) (y h)),
+  div_eq_mul_inv := (λ x y, funext (λ h, @group.div_eq_mul_inv _ (allowable_group γ _) (x h) (y h))),
+  npow := (λ n x h, @group.npow _ (allowable_group γ (h.trans_le hβ)) n (x h)),
+  npow_zero' := (λ x, funext (λ h, @group.npow_zero' _ (allowable_group γ _) (x h))),
+  npow_succ' := (λ n x, funext (λ h, @group.npow_succ' _ (allowable_group γ _) n (x h))),
+  zpow := (λ n x h, @group.zpow _ (allowable_group γ (h.trans_le hβ)) n (x h)),
+  zpow_zero' := (λ x, funext (λ h, @group.zpow_zero' _ (allowable_group γ _) (x h))),
+  zpow_succ' := (λ n x, funext (λ h, @group.zpow_succ' _ (allowable_group γ _) n (x h))),
+  zpow_neg' := (λ n x, funext (λ h, @group.zpow_neg' _ (allowable_group γ _) n (x h))),
+}
+
+instance allowable_group_pi {β : Λ} (hβ : β ≤ α) :
+group (Π γ (h : γ < β), allowable γ (h.trans_le hβ)) :=
+@pi.group _ _ (con_nf.allowable_group_pi' α hβ)
 
 /-- A semiallowable permutation is a `-1`-allowable permutation of atoms (a near-litter permutation)
 together with allowable permutations on all `γ < β`. This forms a group structure automatically. -/
 def semiallowable_perm {β : Λ} (hβ : β ≤ α) :=
 near_litter_perm × Π γ (h : γ < β), allowable γ (h.trans_le hβ)
 
-instance semiallowable_perm_group {β : Λ} (hβ : β ≤ α) : group (semiallowable_perm α hβ) := sorry
--- Use prod.group and allowable_group
+instance semiallowable_perm_group {β : Λ} (hβ : β ≤ α) : group (semiallowable_perm α hβ) :=
+prod.group
 
 instance semiallowable_perm_scalar {β : Λ} (hβ : β ≤ α) :
 has_scalar (semiallowable_perm α hβ) (code α β hβ) := sorry
