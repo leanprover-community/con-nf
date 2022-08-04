e2912c6b923cf4c032621567f4cd4ca9a7582322
amm299 <corp.matthews@protonmail.com>
2022-08-04 04:00:59 +0100

    Worked on mul_action (allowable_path B) (word_support B).

--- src/phase2/strong_support.lean
+++ src/phase2/strong_support.lean
@@ -25,17 +25,40 @@ structure small_strong_support extends strong_support B :=
 
 instance : mul_action (allowable_path B) (word_support B) := {
   smul := λ π S, {
-    carrier := (allowable_path_to_struct_perm B π) • S.carrier,
+    carrier := (π.to_struct_perm) • S.carrier,
     r := λ c₁ c₂, begin
-      refine S.r ⟨(allowable_path_to_struct_perm B π)⁻¹ • c₁, _⟩
-        ⟨(allowable_path_to_struct_perm B π)⁻¹ • c₂, _⟩;
+      refine S.r ⟨(π.to_struct_perm)⁻¹ • c₁, _⟩
+        ⟨(π.to_struct_perm)⁻¹ • c₂, _⟩;
       rw [← set.mem_inv_smul_set_iff, inv_inv],
       exact c₁.property, exact c₂.property,
     end,
-    wo := sorry,
+  wo := { trichotomous := begin intros,
+    have : ∀ c₁ : ↥(allowable_path.to_struct_perm π • S.carrier), (allowable_path.to_struct_perm π)⁻¹ • ↑c₁ ∈ S.carrier,
+    intro c₁, rw [← set.mem_inv_smul_set_iff, inv_inv], exact c₁.property,
+    have : a = b ↔ (⟨(allowable_path.to_struct_perm π)⁻¹ • ↑a, this a⟩ :  {x // x ∈ S.carrier})
+                   =(⟨(allowable_path.to_struct_perm π)⁻¹ • ↑b, this b⟩  :  {x // x ∈ S.carrier}),
+    {split, intro h, subst h, intro h, cases a, cases b,
+    simp only [smul_left_cancel_iff, subtype.coe_mk] at h, subst h},
+    rw this, apply S.wo.trichotomous, end,
+  irrefl := begin intros, apply S.wo.irrefl, end,
+  trans := begin intros,apply S.wo.trans, apply ᾰ, apply ᾰ_1 end,
+  wf := begin have := @inv_image.is_well_founded _ _ S.r ⟨S.wo.wf⟩, convert (this _).wf, end },
   },
-  one_smul := sorry,
-  mul_smul := sorry,
+  one_smul := begin intros, cases b, unfold has_smul.smul, simp only [struct_perm.coe_to_near_litter_perm, map_one, inv_one, one_smul, eq_self_iff_true, true_and],
+  dsimp [(sum.map)], have backup: (↥((1 : allowable_path B) • b_carrier) : Type u) = (↥b_carrier : Type u), rw mul_action.one_smul,have : (↥((1 : allowable_path B) • b_carrier) : Type u) = (↥b_carrier : Type u), rw mul_action.one_smul,convert heq_of_eq _, rw mul_action.one_smul, clear b_wo,
+  suffices : b_r == λ o1 o2, b_r (cast this o1) (cast this o2), exact this,
+  revert b_r this, rw backup,
+  intros, apply heq_of_eq, funext, refl, funext,
+  cases c₁, cases c₂, cases c₁_val, cases c₂_val,
+  simp only [subtype.coe_mk, one_smul, eq_self_iff_true, set_coe_cast], congr,
+  cases c₁_val_fst,
+  simp only [has_smul.comp.smul, sum.elim_inl, function.comp_app, struct_perm.of_bot_one, one_smul],
+  simp only [has_smul.comp.smul, sum.elim_inr, function.comp_app, struct_perm.of_bot_one, one_smul],
+  cases c₂_val_fst,
+  simp only [has_smul.comp.smul, sum.elim_inl, function.comp_app, struct_perm.of_bot_one, one_smul],
+  simp only [has_smul.comp.smul, sum.elim_inr, function.comp_app, struct_perm.of_bot_one, one_smul],
+  end,
+  mul_smul := begin intros, cases b, unfold has_smul.smul, sorry end,
 }
 
 /-- We can lower a support to a lower proper type index with respect to a path
