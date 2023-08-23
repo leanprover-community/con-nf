5d19001220e2968b84a33b7bb681f55ce3bb84dd
zeramorphic <zeramorphic@proton.me>
2023-08-23 22:47:44 +0100

    Port AtomCompletion
    Signed-off-by: zeramorphic <zeramorphic@proton.me>

--- ConNF/Phase2/AtomCompletion.lean
+++ ConNF/Phase2/AtomCompletion.lean
@@ -1,7 +1,5 @@
 import ConNF.Phase2.Approximation
 
-#align_import phase2.atom_completion
-
 open Set Sum
 
 open scoped Classical
@@ -17,23 +15,21 @@ namespace StructApprox
 open NearLitterApprox Hypothesis
 
 theorem orderIso_apply_mem {S T : Sublitter} {a} {L : Litter}
-    (h : (S.OrderIso T a : Atom) ∈ litterSet L) : T.Litter = L :=
-  by
-  rw [← litter.litter_to_sublitter L, ← sublitter.inter_nonempty_iff]
-  exact ⟨_, (S.order_iso T a).prop, h⟩
+    (h : (S.orderIso T a : Atom) ∈ litterSet L) : T.litter = L := by
+  rw [← Litter.litter_toSublitter L, ← Sublitter.inter_nonempty_iff]
+  exact ⟨_, (S.orderIso T a).prop, h⟩
 
 theorem orderIso_apply_eq {S T U V : Sublitter} {a b}
-    (h : (S.OrderIso T a : Atom) = U.OrderIso V b) : T.Litter = V.Litter :=
-  by
+    (h : (S.orderIso T a : Atom) = U.orderIso V b) : T.litter = V.litter := by
   rw [Subtype.coe_eq_iff] at h
-  exact (order_iso_apply_mem (T.subset h.some)).symm
+  exact (orderIso_apply_mem (T.subset h.choose)).symm
 
 /-- Computes the action of a structural approximation `π` on an atom `a`. -/
 noncomputable def atomCompletion (A : ExtendedIndex β) (a : Atom) (H : Hypothesis ⟨inl a, A⟩) :
     Atom :=
   if h : a ∈ (π A).atomPerm.domain then π A • a
   else
-    ((π A).largestSublitter a.1).OrderIso
+    ((π A).largestSublitter a.1).orderIso
       ((π A).largestSublitter
         (H.nearLitterImage A a.1.toNearLitter (Relation.TransGen.single <| Constrains.atom a A)).1)
       ⟨a, (π A).mem_largestSublitter_of_not_mem_domain a h⟩
