9225c2146bc3ee6b3bbbf10eafdd9d1d2497c52d
zeramorphic <zeramorphic@proton.me>
2023-08-23 22:55:22 +0100

    Port FlexibleCompletion
    Signed-off-by: zeramorphic <zeramorphic@proton.me>

--- ConNF/Phase2.lean
+++ ConNF/Phase2.lean
@@ -8,7 +8,7 @@ import ConNF.Phase2.Reduction
 import ConNF.Phase2.NearLitterAction
 import ConNF.Phase2.StructAction
 import ConNF.Phase2.AtomCompletion
--- import ConNF.Phase2.FlexibleCompletion
+import ConNF.Phase2.FlexibleCompletion
 -- import ConNF.Phase2.NearLitterCompletion
 -- import ConNF.Phase2.FillAtomRange
 -- import ConNF.Phase2.CompleteAction
