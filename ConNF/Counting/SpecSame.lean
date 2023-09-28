e2341d765ebbfe03228dc6dd9790e9165dd500a3
zeramorphic <zeramorphic@proton.me>
2023-09-28 17:42:12 +0100

    convertAllowable_smul setup
    Signed-off-by: zeramorphic <zeramorphic@proton.me>

--- ConNF/Counting/SpecSame.lean
+++ ConNF/Counting/SpecSame.lean
@@ -22,6 +22,10 @@ namespace Spec
 theorem type_eq_type : type S.r = type T.r :=
   by rw [← orderType_eq_of_specifies hσS, ← orderType_eq_of_specifies hσT]
 
+theorem typein_lt {c : S} :
+    typein S.r c < σ.orderType :=
+  orderType_eq_of_specifies hσS ▸ typein_lt_type S.r _
+
 noncomputable def convertCondition (c : S) : T :=
   T.conditionAt (typein S.r c) (type_eq_type hσS hσT ▸ typein_lt_type S.r c)
 
@@ -29,6 +33,13 @@ noncomputable def convertCondition (c : S) : T :=
 theorem typein_convertCondition (c : S) :
     typein T.r (convertCondition hσS hσT c) = typein S.r c :=
   by rw [convertCondition, typein_conditionAt]
+
+@[simp]
+theorem convertCondition_lt (c d : S) :
+    convertCondition hσS hσT c < convertCondition hσS hσT d ↔ c < d := by
+  refine (typein_lt_typein T.r).symm.trans ?_
+  simp only [OrdSupport.coe_sort_coe, typein_convertCondition, typein_lt_typein]
+  rfl
 
 @[simp]
 theorem convertCondition_convertCondition (c : S) :
@@ -147,6 +158,23 @@ theorem convertCondition_eq_convertLitter (A : ExtendedIndex β) (L : Litter)
     convertCondition hσS hσT ⟨⟨A, inr L.toNearLitter⟩, h⟩ =
     ⟨⟨A, inr (convertLitter hσS hσT hT A L h).toNearLitter⟩, (convertLitter_mem hσS hσT hT A L h)⟩ :=
   (convertCondition_litter hσS hσT hT A L h).choose_spec.2
+
+@[simp]
+theorem convertCondition_path (c : S) :
+    (convertCondition hσS hσT c).val.path = c.val.path := by
+  obtain ⟨⟨A, a | N⟩, hc⟩ := c
+  · rw [convertCondition_eq_convertAtom hσS hσT hS hT]
+  · obtain ⟨L, rfl⟩ := (isLitter_of_reduced (hS.reduced_of_mem ⟨_, hc⟩)).exists_litter_eq
+    rw [convertCondition_eq_convertLitter hσS hσT hT]
+
+@[simp]
+theorem convertCondition_mk (A : ExtendedIndex β) (x : Atom ⊕ NearLitter) (h : ⟨A, x⟩ ∈ S) :
+    ⟨A, (convertCondition hσS hσT ⟨⟨A, x⟩, h⟩).val.value⟩ =
+    (convertCondition hσS hσT ⟨⟨A, x⟩, h⟩).val := by
+  have := convertCondition_path hσS hσT hS hT ⟨⟨A, x⟩, h⟩
+  dsimp only at this
+  conv in SupportCondition.mk _ =>
+    rw [← this]
 
 @[simp]
 theorem convertAtom_convertAtom (A : ExtendedIndex β) (a : Atom) (h : ⟨A, inl a⟩ ∈ S) :
@@ -290,6 +318,188 @@ theorem convertAllowable_spec :
   (StructApprox.freedom_of_action β
     (StructAction.rc (convert hσS hσT hS hT) (convert_lawful hσS hσT hS hT))
     (StructAction.rc_free _ _ (convert_mapFlexible hσS hσT hS hT))).choose_spec
+
+/-- If `LawfulIn s` holds, `convertAllowable` acts on support conditions assigned at a time in `s`
+exactly as specified by `convertCondition`. -/
+structure LawfulIn (s : Set Ordinal) : Prop where
+  smul_eq : ∀ c : S, typein S.r c ∈ s →
+    convertAllowable hσS hσT hS hT • c.val = convertCondition hσS hσT c
+  inv_smul_eq : ∀ c : T, typein T.r c ∈ s →
+    (convertAllowable hσS hσT hS hT)⁻¹ • c.val = convertCondition hσT hσS c
+
+theorem LawfulIn.smul_mem {s : Set Ordinal} (h : LawfulIn hσS hσT hS hT s)
+    (c : S) (hc : typein S.r c ∈ s) :
+    convertAllowable hσS hσT hS hT • c.val ∈ T := by
+  rw [h.smul_eq c hc]
+  exact (convertCondition hσS hσT c).prop
+
+theorem LawfulIn.inv_smul_mem {s : Set Ordinal} (h : LawfulIn hσS hσT hS hT s)
+    (c : T) (hc : typein T.r c ∈ s) :
+    (convertAllowable hσS hσT hS hT)⁻¹ • c.val ∈ S := by
+  rw [h.inv_smul_eq c hc]
+  exact (convertCondition hσT hσS c).prop
+
+theorem LawfulIn.typein_smul {s : Set Ordinal} (h : LawfulIn hσS hσT hS hT s)
+    (c : S) (hc : typein S.r c ∈ s) :
+    typein T.r ⟨convertAllowable hσS hσT hS hT • c.val, h.smul_mem c hc⟩ = typein S.r c :=
+  by simp only [h.smul_eq c hc, OrdSupport.coe_sort_coe, Subtype.coe_eta, typein_convertCondition]
+
+theorem LawfulIn.typein_inv_smul {s : Set Ordinal} (h : LawfulIn hσS hσT hS hT s)
+    (c : T) (hc : typein T.r c ∈ s) :
+    typein S.r ⟨(convertAllowable hσS hσT hS hT)⁻¹ • c.val, h.inv_smul_mem c hc⟩ = typein T.r c :=
+  by simp only [h.inv_smul_eq c hc, OrdSupport.coe_sort_coe, Subtype.coe_eta,
+    typein_convertCondition]
+
+abbrev LawfulBefore (i : Ordinal) : Prop :=
+  LawfulIn hσS hσT hS hT {j | j < i}
+
+theorem lawfulIn_iff (s : Set Ordinal) :
+    LawfulIn hσS hσT hS hT s ↔ ∀ i ∈ s, LawfulIn hσS hσT hS hT {i} := by
+  constructor
+  · intro h i hi
+    constructor
+    case smul_eq =>
+      rintro c rfl
+      exact h.smul_eq c hi
+    case inv_smul_eq =>
+      rintro c rfl
+      exact h.inv_smul_eq c hi
+  · intro h
+    constructor
+    case smul_eq =>
+      intro c hcd
+      refine (h _ hcd).smul_eq c rfl
+    case inv_smul_eq =>
+      intro c hcd
+      refine (h _ hcd).inv_smul_eq c rfl
+
+theorem lawfulBefore_induction
+    (h : ∀ i, LawfulBefore hσS hσT hS hT i → LawfulIn hσS hσT hS hT {i}) :
+    LawfulIn hσS hσT hS hT univ := by
+  rw [lawfulIn_iff]
+  simp only [mem_univ, forall_true_left]
+  intro i
+  induction i using Ordinal.induction with
+  | h j ih =>
+    refine h j ?_
+    rw [LawfulBefore, lawfulIn_iff]
+    exact ih
+
+theorem mem_before_smul_iff_mem_before (A : ExtendedIndex β) (i : Ordinal)
+    (P : InflexibleCoePath A) (ih : LawfulBefore hσS hσT hS hT i)
+    (c : SupportCondition P.δ) :
+    c ∈ (S.before i).comp P.δ (P.B.cons (coe_lt P.hδ)) ↔
+    Tree.comp (P.B.cons (coe_lt P.hδ))
+        (Allowable.toStructPerm (convertAllowable hσS hσT hS hT)) • c ∈
+      ((T.before i).comp P.δ (P.B.cons (coe_lt P.hδ))) := by
+  constructor
+  · rintro ⟨h₁, h₂⟩
+    refine ⟨ih.smul_mem ⟨_, h₁⟩ h₂, ?_⟩
+    change typein T.r ⟨convertAllowable hσS hσT hS hT •
+      ⟨(P.B.cons (coe_lt P.hδ)).comp c.path, c.value⟩, _⟩ < _
+    rw [ih.typein_smul ⟨_, h₁⟩ h₂]
+    exact h₂
+  · rintro ⟨h₁, h₂⟩
+    change convertAllowable hσS hσT hS hT •
+      ⟨(P.B.cons (coe_lt P.hδ)).comp c.path, c.value⟩ ∈ T at h₁
+    change typein T.r ⟨(convertAllowable hσS hσT hS hT •
+      ⟨(P.B.cons (coe_lt P.hδ)).comp c.path, c.value⟩), _⟩ < _ at h₂
+    refine ⟨?_, ?_⟩
+    · have := ih.inv_smul_mem ⟨_, h₁⟩ h₂
+      rw [inv_smul_smul] at this
+      exact this
+    · simp only [← ih.typein_inv_smul ⟨_, h₁⟩ h₂, inv_smul_smul] at h₂
+      exact h₂
+
+theorem before_smul_eq_before (A : ExtendedIndex β) (i : Ordinal)
+    (P : InflexibleCoePath A) (ih : LawfulBefore hσS hσT hS hT i) :
+    (S.before i).comp P.δ (P.B.cons (coe_lt P.hδ)) =
+    (show Allowable (P.δ : Iic α) from
+      (Allowable.comp (show Path ((β : IicBot α) : TypeIndex) (P.δ : IicBot α) from
+          P.B.cons (coe_lt P.hδ)))
+        (convertAllowable hσS hσT hS hT))⁻¹ •
+    ((T.before i).comp P.δ (P.B.cons (coe_lt P.hδ))) := by
+  dsimp only
+  refine OrdSupport.ext ?_ ?_ ?_
+  · intro c
+    rw [mem_before_smul_iff_mem_before hσS hσT hS hT A i P ih c,
+      OrdSupport.smul_mem, inv_inv, Allowable.smul_supportCondition,
+      Allowable.toStructPerm_comp (show Path ((β : IicBot α) : TypeIndex) (P.δ : IicBot α) from
+        P.B.cons (coe_lt P.hδ))]
+    exact id
+  · intro c
+    rw [mem_before_smul_iff_mem_before hσS hσT hS hT A i P ih c,
+      OrdSupport.smul_mem, inv_inv, Allowable.smul_supportCondition,
+      Allowable.toStructPerm_comp (show Path ((β : IicBot α) : TypeIndex) (P.δ : IicBot α) from
+        P.B.cons (coe_lt P.hδ))]
+    exact id
+  · intro c d
+    have hc := ih.smul_eq ⟨_, c.prop.1⟩ c.prop.2
+    have hd := ih.smul_eq ⟨_, d.prop.1⟩ d.prop.2
+    simp only [Allowable.smul_supportCondition, SupportCondition.ext_iff] at hc hd
+    have := Allowable.toStructPerm_comp
+      (show Path ((β : IicBot α) : TypeIndex) (P.δ : IicBot α) from P.B.cons (coe_lt P.hδ))
+      (convertAllowable hσS hσT hS hT)
+    dsimp only at this
+    simp only [OrdSupport.comp_lt, OrdSupport.before_lt, OrdSupport.lt_iff_smul, inv_inv,
+      Allowable.smul_supportCondition, this, Tree.comp_apply, hc.2, hd.2,
+      convertCondition_mk hσS hσT hS hT ((P.B.cons (coe_lt P.hδ)).comp c.val.path),
+      convertCondition_mk hσS hσT hS hT ((P.B.cons (coe_lt P.hδ)).comp d.val.path),
+      Subtype.coe_eta, convertCondition_lt]
+
+theorem spec_inflexibleBot (A : ExtendedIndex β) (L : Litter) (hL : InflexibleBot A L)
+    (haS : ⟨hL.path.B.cons (bot_lt_coe _), inl hL.a⟩ ∈ S) (hLS₁ : ⟨A, inr L.toNearLitter⟩ ∈ S)
+    (hLS₂ : σ.cond (typein S.r ⟨_, hLS₁⟩) (typein_lt hσS) =
+      SpecCondition.inflexibleBot A hL.path (typein S.r ⟨_, haS⟩)) :
+    ∃ hL' : InflexibleBot A (convertLitter hσS hσT hT A L hLS₁),
+    ∃ haT : ⟨hL'.path.B.cons (bot_lt_coe _), inl hL'.a⟩ ∈ T,
+      hL.path = hL'.path ∧ typein S.r ⟨_, haS⟩ = typein T.r ⟨_, haT⟩ := by
+  obtain (hL' | ⟨⟨hL'⟩⟩ | ⟨⟨hL'⟩⟩) := flexible_cases' β A (convertLitter hσS hσT hT A L hLS₁)
+  · have := hσT.flexible_spec A (convertLitter hσS hσT hT A L hLS₁).toNearLitter
+      (convertLitter_mem hσS hσT hT A L hLS₁) hL'
+    simp only [OrdSupport.coe_sort_coe, typein_convertLitter] at this
+    rw [hLS₂] at this
+    cases this
+  · obtain ⟨_, this⟩ := hσT.inflexibleBot_spec A
+      (convertLitter hσS hσT hT A L hLS₁).toNearLitter
+      (convertLitter_mem hσS hσT hT A L hLS₁) hL'
+    simp only [OrdSupport.coe_sort_coe, typein_convertLitter] at this
+    rw [hLS₂] at this
+    exact ⟨hL', _, SpecCondition.inflexibleBot_injective this⟩
+  · obtain ⟨_, _, _, this⟩ := hσT.inflexibleCoe_spec A
+      (convertLitter hσS hσT hT A L hLS₁).toNearLitter
+      (convertLitter_mem hσS hσT hT A L hLS₁) hL'
+    simp only [OrdSupport.coe_sort_coe, typein_convertLitter] at this
+    rw [hLS₂] at this
+    cases this
+
+theorem lawfulIn_step (i : Ordinal) (ih : LawfulBefore hσS hσT hS hT i) :
+    LawfulIn hσS hσT hS hT {i} := by
+  constructor
+  case smul_eq =>
+    rintro c rfl
+    sorry
+  case inv_smul_eq =>
+    rintro c rfl
+    sorry
+
+theorem lawfulIn_all : LawfulIn hσS hσT hS hT univ :=
+  lawfulBefore_induction hσS hσT hS hT (lawfulIn_step hσS hσT hS hT)
+
+theorem convertAllowable_smul : convertAllowable hσS hσT hS hT • S = T := by
+  refine OrdSupport.ext ?_ ?_ ?_
+  · intro c hc
+    have := (lawfulIn_all hσS hσT hS hT).smul_mem ⟨_, hc⟩ (mem_univ _)
+    rw [smul_inv_smul] at this
+    exact this
+  · intro c hc
+    exact (lawfulIn_all hσS hσT hS hT).inv_smul_mem ⟨_, hc⟩ (mem_univ _)
+  · intro c d
+    have hc := (lawfulIn_all hσS hσT hS hT).smul_eq ⟨_, c.prop⟩ (mem_univ _)
+    have hd := (lawfulIn_all hσS hσT hS hT).smul_eq ⟨_, d.prop⟩ (mem_univ _)
+    simp only [smul_inv_smul] at hc hd
+    conv_rhs => simp (config := { singlePass := true }) only [hc, hd]
+    simp only [OrdSupport.lt_iff_smul, Subtype.coe_eta, convertCondition_lt]
 
 end Spec
 
