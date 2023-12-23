6e9825dc2c5b9ed8e11cf0923fc397f9c8be73d9
zeramorphic <zeramorphic@proton.me>
2023-12-23 18:24:17 +0000

    Temporary fix for lake manifest
    Signed-off-by: zeramorphic <zeramorphic@proton.me>

--- update_mathlib.sh
+++ update_mathlib.sh
@@ -1,4 +1,3 @@
 # Update mathlib and the lean toolchain.
 curl -L https://raw.githubusercontent.com/leanprover-community/mathlib4/master/lean-toolchain -o lean-toolchain
 lake -Kenv=dev update
-lake exe cache get
