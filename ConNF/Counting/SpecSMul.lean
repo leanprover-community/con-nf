8fe1812134ea93a3b2edb0bdc58cc0a94913b91d
zeramorphic <zeramorphic@proton.me>
2024-03-14 22:49:07 +0000

    Bump mathlib
    Signed-off-by: zeramorphic <zeramorphic@proton.me>

--- ConNF/Counting/SpecSMul.lean
+++ ConNF/Counting/SpecSMul.lean
@@ -11,6 +11,5 @@ universe u
 namespace ConNF
 
 variable [Params.{u}] [Level] [FOAAssumptions] {β : Λ} [i : LeLevel β] {σ : Spec β} {S : Support β}
-    {lS : ∀ {γ : Λ}, (hγ : (γ : TypeIndex) < β) → (A : Path (β : TypeIndex) γ) → Support γ}
 
 end ConNF
