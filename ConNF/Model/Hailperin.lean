aefecf0384efd92cc8b23d5bf0f476b4d2272c49
zeramorphic <zeramorphic@proton.me>
2024-03-31 10:31:50 +0100

    Finish predicative part of Hailperin
    Signed-off-by: zeramorphic <zeramorphic@proton.me>

--- ConNF/Model/Hailperin.lean
+++ ConNF/Model/Hailperin.lean
@@ -143,4 +143,183 @@ def TangleData.TSet.singletonImage (t : TSet β) : TSet α :=
   mk hβ {p | ∃ a b : TSet ε, op hδ hε a b ∈[hγ] t ∧
     p = op hγ hδ (singleton hε a) (singleton hε b)} (singletonImage_supported hβ hγ hδ hε t)
 
+theorem Symmetric.insertion2 {s : Set (TSet δ)} (h : Symmetric hδ s) :
+    Symmetric hβ {p | ∃ a b c : TSet ζ, op hε hζ a c ∈ s ∧
+      p = op hγ hδ (.singleton hε (.singleton hζ a)) (op hε hζ b c)} := by
+  obtain ⟨S, hS, hS'⟩ := h
+  refine Symmetric.ofSubset hβ _ ?_
+  refine ⟨raise (coe_lt_coe.mpr hβ) ∘ raise (coe_lt_coe.mpr hγ) '' S, hS.image, ?_⟩
+  intro ρ hρ
+  have := hS' (ρ |> cons hβ |> cons hγ) ?_
+  swap
+  · simp only [Set.mem_image, Allowable.smul_address_eq_iff, forall_exists_index, and_imp,
+      forall_apply_eq_imp_iff₂, raise_path, raise_value, cons_toStructPerm, Tree.comp_apply] at hρ ⊢
+    exact hρ
+  rintro _ ⟨_, ⟨a, b, c, habc, rfl⟩, rfl⟩
+  refine ⟨(ρ |> cons hβ |> cons hγ |> cons hδ |> cons hε |> cons hζ) • a,
+      (ρ |> cons hβ |> cons hγ |> cons hδ |> cons hε |> cons hζ) • b,
+      (ρ |> cons hβ |> cons hγ |> cons hδ |> cons hε |> cons hζ) • c, ?_, ?_⟩
+  · rw [← this, Set.mem_smul_set_iff_inv_smul_mem]
+    simp only [smul_op, map_inv, inv_smul_smul]
+    exact habc
+  · simp only [smul_op, smul_singleton]
+
+theorem TangleData.TSet.insertion2_supported (t : TSet γ) :
+    Symmetric hβ {p | ∃ a b c : TSet ζ, op hε hζ a c ∈[hδ] t ∧
+      p = op hγ hδ (.singleton hε (.singleton hζ a)) (op hε hζ b c)} := by
+  obtain ⟨s, hs, rfl⟩ := t.induction hδ
+  simp only [mem_mk_iff]
+  exact hs.insertion2 _ _ _ _
+
+def TangleData.TSet.insertion2 (t : TSet γ) : TSet α :=
+  mk hβ {p | ∃ a b c : TSet ζ, op hε hζ a c ∈[hδ] t ∧
+    p = op hγ hδ (.singleton hε (.singleton hζ a)) (op hε hζ b c)}
+    (insertion2_supported hβ hγ hδ hε hζ t)
+
+theorem Symmetric.insertion3 {s : Set (TSet δ)} (h : Symmetric hδ s) :
+    Symmetric hβ {p | ∃ a b c : TSet ζ, op hε hζ a b ∈ s ∧
+      p = op hγ hδ (.singleton hε (.singleton hζ a)) (op hε hζ b c)} := by
+  obtain ⟨S, hS, hS'⟩ := h
+  refine Symmetric.ofSubset hβ _ ?_
+  refine ⟨raise (coe_lt_coe.mpr hβ) ∘ raise (coe_lt_coe.mpr hγ) '' S, hS.image, ?_⟩
+  intro ρ hρ
+  have := hS' (ρ |> cons hβ |> cons hγ) ?_
+  swap
+  · simp only [Set.mem_image, Allowable.smul_address_eq_iff, forall_exists_index, and_imp,
+      forall_apply_eq_imp_iff₂, raise_path, raise_value, cons_toStructPerm, Tree.comp_apply] at hρ ⊢
+    exact hρ
+  rintro _ ⟨_, ⟨a, b, c, habc, rfl⟩, rfl⟩
+  refine ⟨(ρ |> cons hβ |> cons hγ |> cons hδ |> cons hε |> cons hζ) • a,
+      (ρ |> cons hβ |> cons hγ |> cons hδ |> cons hε |> cons hζ) • b,
+      (ρ |> cons hβ |> cons hγ |> cons hδ |> cons hε |> cons hζ) • c, ?_, ?_⟩
+  · rw [← this, Set.mem_smul_set_iff_inv_smul_mem]
+    simp only [smul_op, map_inv, inv_smul_smul]
+    exact habc
+  · simp only [smul_op, smul_singleton]
+
+theorem TangleData.TSet.insertion3_supported (t : TSet γ) :
+    Symmetric hβ {p | ∃ a b c : TSet ζ, op hε hζ a b ∈[hδ] t ∧
+      p = op hγ hδ (.singleton hε (.singleton hζ a)) (op hε hζ b c)} := by
+  obtain ⟨s, hs, rfl⟩ := t.induction hδ
+  simp only [mem_mk_iff]
+  exact hs.insertion3 _ _ _ _
+
+def TangleData.TSet.insertion3 (t : TSet γ) : TSet α :=
+  mk hβ {p | ∃ a b c : TSet ζ, op hε hζ a b ∈[hδ] t ∧
+    p = op hγ hδ (.singleton hε (.singleton hζ a)) (op hε hζ b c)}
+    (insertion3_supported hβ hγ hδ hε hζ t)
+
+theorem Symmetric.cross {s : Set (TSet δ)} (h : Symmetric hδ s) :
+    Symmetric hβ {p | ∃ a b : TSet δ, b ∈ s ∧ p = op hγ hδ a b} := by
+  obtain ⟨S, hS, hS'⟩ := h
+  refine Symmetric.ofSubset hβ _ ?_
+  refine ⟨raise (coe_lt_coe.mpr hβ) ∘ raise (coe_lt_coe.mpr hγ) '' S, hS.image, ?_⟩
+  intro ρ hρ
+  have := hS' (ρ |> cons hβ |> cons hγ) ?_
+  swap
+  · simp only [Set.mem_image, Allowable.smul_address_eq_iff, forall_exists_index, and_imp,
+      forall_apply_eq_imp_iff₂, raise_path, raise_value, cons_toStructPerm, Tree.comp_apply] at hρ ⊢
+    exact hρ
+  rintro _ ⟨_, ⟨a, b, hb, rfl⟩, rfl⟩
+  refine ⟨(ρ |> cons hβ |> cons hγ |> cons hδ) • a,
+      (ρ |> cons hβ |> cons hγ |> cons hδ) • b, ?_, ?_⟩
+  · rw [← this, Set.mem_smul_set_iff_inv_smul_mem, inv_smul_smul]
+    exact hb
+  · simp only [smul_op]
+
+theorem TangleData.TSet.cross_supported (t : TSet γ) :
+    Symmetric hβ {p | ∃ a b : TSet δ, b ∈[hδ] t ∧ p = op hγ hδ a b} := by
+  obtain ⟨s, hs, rfl⟩ := t.induction hδ
+  simp only [mem_mk_iff]
+  exact hs.cross _ _ _
+
+def TangleData.TSet.cross (t : TSet γ) : TSet α :=
+  mk hβ {p | ∃ a b : TSet δ, b ∈[hδ] t ∧ p = op hγ hδ a b}
+    (cross_supported hβ hγ hδ t)
+
+theorem Symmetric.typeLower' {s : Set (TSet β)} (h : Symmetric hβ s) :
+    Symmetric hβ {p | ∃ a : TSet ε, (∀ b : TSet δ, op hγ hδ b (.singleton hε a) ∈ s)
+      ∧ p = .singleton hγ (.singleton hδ (.singleton hε a))} := by
+  obtain ⟨S, hS, hS'⟩ := h
+  refine Symmetric.ofSubset hβ _ ?_
+  refine ⟨S, hS, ?_⟩
+  intro ρ hρ
+  have := hS' ρ hρ
+  rintro _ ⟨_, ⟨a, ha, rfl⟩, rfl⟩
+  refine ⟨(ρ |> cons hβ |> cons hγ |> cons hδ |> cons hε) • a, ?_, ?_⟩
+  · intro b
+    rw [← this, Set.mem_smul_set_iff_inv_smul_mem]
+    simp only [smul_op, map_inv, smul_singleton, inv_smul_smul]
+    exact ha _
+  · simp only [smul_singleton]
+
+theorem TangleData.TSet.typeLower'_supported (t : TSet α) :
+    Symmetric hβ {p | ∃ a : TSet ε, (∀ b : TSet δ, op hγ hδ b (.singleton hε a) ∈[hβ] t)
+      ∧ p = .singleton hγ (.singleton hδ (.singleton hε a))} := by
+  obtain ⟨s, hs, rfl⟩ := t.induction hβ
+  simp only [mem_mk_iff]
+  exact hs.typeLower' _ _ _
+
+/-- This isn't quite the right form of the type lowering axiom, but once we have the axiom
+of union for singletons, we will be able to deduce the correct form of the result. -/
+def TangleData.TSet.typeLower' (t : TSet α) : TSet α :=
+  mk hβ {p | ∃ a : TSet ε, (∀ b : TSet δ, op hγ hδ b (.singleton hε a) ∈[hβ] t)
+      ∧ p = .singleton hγ (.singleton hδ (.singleton hε a))}
+    (typeLower'_supported hβ hγ hδ hε t)
+
+theorem Symmetric.converse {s : Set (TSet β)} (h : Symmetric hβ s) :
+    Symmetric hβ {p | ∃ a b : TSet δ, op hγ hδ a b ∈ s ∧ p = op hγ hδ b a} := by
+  obtain ⟨S, hS, hS'⟩ := h
+  refine Symmetric.ofSubset hβ _ ?_
+  refine ⟨S, hS, ?_⟩
+  intro ρ hρ
+  have := hS' ρ hρ
+  rintro _ ⟨_, ⟨a, b, hab, rfl⟩, rfl⟩
+  refine ⟨(ρ |> cons hβ |> cons hγ |> cons hδ) • a,
+      (ρ |> cons hβ |> cons hγ |> cons hδ) • b, ?_, ?_⟩
+  · rw [← this, Set.mem_smul_set_iff_inv_smul_mem]
+    simp only [smul_op, map_inv, smul_singleton, inv_smul_smul]
+    exact hab
+  · simp only [smul_op]
+
+theorem TangleData.TSet.converse_supported (t : TSet α) :
+    Symmetric hβ {p | ∃ a b : TSet δ, op hγ hδ a b ∈[hβ] t ∧ p = op hγ hδ b a} := by
+  obtain ⟨s, hs, rfl⟩ := t.induction hβ
+  simp only [mem_mk_iff]
+  exact hs.converse _ _ _
+
+def TangleData.TSet.converse (t : TSet α) : TSet α :=
+  mk hβ {p | ∃ a b : TSet δ, op hγ hδ a b ∈[hβ] t ∧ p = op hγ hδ b a}
+    (converse_supported hβ hγ hδ t)
+
+theorem Symmetric.cardinalOne :
+    Symmetric hβ {p | ∃ a : TSet γ, p = .singleton hγ a} := by
+  refine Symmetric.ofSubset hβ _ ?_
+  refine ⟨∅, small_empty, ?_⟩
+  intro ρ _
+  rintro _ ⟨_, ⟨a, rfl⟩, rfl⟩
+  simp only [smul_singleton, Set.mem_setOf_eq]
+  exact ⟨_, rfl⟩
+
+def TangleData.TSet.cardinalOne : TSet α :=
+  mk hβ {p | ∃ a : TSet γ, p = .singleton hγ a} (Symmetric.cardinalOne hβ hγ)
+
+theorem Symmetric.subset :
+    Symmetric hβ {p | ∃ a b : TSet δ, (∀ c : TSet ε, c ∈[hε] a → c ∈[hε] b) ∧
+      p = op hγ hδ a b} := by
+  refine Symmetric.ofSubset hβ _ ?_
+  refine ⟨∅, small_empty, ?_⟩
+  intro ρ _
+  rintro _ ⟨_, ⟨a, b, ha, rfl⟩, rfl⟩
+  simp only [smul_op, Set.mem_setOf_eq]
+  refine ⟨_, _, ?_, rfl⟩
+  intro c hc
+  have := ha ((ρ |> cons hβ |> cons hγ |> cons hδ |> cons hε)⁻¹ • c)
+  rw [← mem_smul_iff, ← mem_smul_iff] at this
+  exact this hc
+
+def TangleData.TSet.subset : TSet α :=
+  mk hβ {p | ∃ a b : TSet δ, (∀ c : TSet ε, c ∈[hε] a → c ∈[hε] b) ∧
+      p = op hγ hδ a b} (Symmetric.subset hβ hγ hδ hε)
+
 end ConNF
