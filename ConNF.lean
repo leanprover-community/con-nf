f0d109557b95e745d3b161929653f1d0c16a4860
zeramorphic <zeramorphic@proton.me>
2024-07-16 22:51:13 +0100

    Create blueprint
    Signed-off-by: zeramorphic <zeramorphic@proton.me>

--- ConNF.lean
+++ ConNF.lean
@@ -1,8 +1,1 @@
 import ConNF.Mathlib
-import ConNF.BaseType
-import ConNF.Structural
-import ConNF.Fuzz
-import ConNF.Construction
-import ConNF.FOA
-import ConNF.Counting
-import ConNF.Model
---
+++ tasks.py
@@ -0,0 +1,50 @@
+import os
+import shutil
+import subprocess
+from pathlib import Path
+from invoke import run, task
+
+from blueprint.tasks import web, bp, serve
+
+ROOT = Path(__file__).parent
+BP_DIR = ROOT/'blueprint'
+
+@task(bp, web)
+def all(ctx):
+    shutil.rmtree(ROOT/'docs'/'blueprint', ignore_errors=True)
+    shutil.copytree(ROOT/'blueprint'/'web', ROOT/'docs'/'blueprint')
+    shutil.copy2(ROOT/'blueprint'/'print'/'print.pdf', ROOT/'docs'/'blueprint.pdf')
+
+@task(web)
+def html(ctx):
+    shutil.rmtree(ROOT/'docs'/'blueprint', ignore_errors=True)
+    shutil.copytree(ROOT/'blueprint'/'web', ROOT/'docs'/'blueprint')
+
+@task(bp, web)
+def dev(ctx):
+    """
+    Serve the blueprint website, rebuild PDF and the website on file changes
+    """
+
+    from watchfiles import run_process, DefaultFilter
+
+    def callback(changes):
+        print('Changes detected:', changes)
+        bp(ctx)
+        web(ctx)
+
+    run_process(BP_DIR/'src', target='inv serve', callback=callback,
+        watch_filter=DefaultFilter(
+            ignore_entity_patterns=(
+                '.*\.aux$',
+                '.*\.log$',
+                '.*\.fls$',
+                '.*\.fdb_latexmk$',
+                '.*\.bbl$',
+                '.*\.paux$',
+                '.*\.pdf$',
+                '.*\.out$',
+                '.*\.blg$',
+                '.*\.synctex.*$',
+            )
+        ))
