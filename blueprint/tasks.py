3761999972da60601f103a529b7807def54401f5
zeramorphic <zeramorphic@proton.me>
2024-08-12 14:32:53 +0100

    Split blueprint into several files
    Signed-off-by: zeramorphic <zeramorphic@proton.me>

--- blueprint/tasks.py
+++ blueprint/tasks.py
@@ -1,5 +1,6 @@
 import os
 import random
+import re
 from pathlib import Path
 import http.server
 import socketserver
@@ -25,18 +26,48 @@ def web(ctx):
     os.chdir(BP_DIR/'src')
     run('plastex -c plastex.cfg web.tex')
     os.chdir(BP_DIR/'web')
-    with open('dep_graph_document.html', 'r') as f:
-        s = f.read()
-    with open('dep_graph_document.html', 'w') as f:
-        def replace(line):
-            if ".renderDot" in line:
-                return line.replace('[', '[fontname="Open Sans",')
-            elif "theme-white.css" in line:
-                return line + '\n<link rel="stylesheet" href="styles/extra_styles.css" />'
-            else:
-                return line
-        s = '\n'.join(replace(line) for line in s.split('\n'))
-        f.write(s)
+
+    nonempty_graphs = []
+    for file in os.listdir('.'):
+        filename = os.fsdecode(file)
+        if filename.startswith("dep_graph"):
+            with open(filename, 'r') as f:
+                s = f.read()
+                for line in s.split('\n'):
+                    if ".renderDot" in line:
+                        if re.search('"[a-zA-Z]', line):
+                            # This is a good heuristic for whether a graph has a node or not.
+                            nonempty_graphs.append(filename)
+            with open(filename, 'w') as f:
+                def replace(line):
+                    if ".renderDot" in line:
+                        # Replace the font with Open Sans.
+                        return line.replace('[', '[fontname="Open Sans",')
+                    elif "theme-white.css" in line:
+                        return line + '\n<link rel="stylesheet" href="styles/extra_styles.css" />'
+                    else:
+                        return line
+                s = '\n'.join(replace(line) for line in s.split('\n'))
+                f.write(s)
+
+    for file in os.listdir('.'):
+        filename = os.fsdecode(file)
+        if filename.endswith(".html"):
+            with open(filename, 'r') as f:
+                s = f.read()
+            with open(filename, 'w') as f:
+                # Remove empty dependency graphs.
+                to_remove = []
+                for match in re.finditer(r'<li ><a href="([a-zA-Z0-9_.]*)">Chapter [a-zA-Z0-9]* graph<\/a><\/li>', s):
+                    if match.group(1) not in nonempty_graphs:
+                        to_remove.append(match.group(0))
+                for item in to_remove:
+                    s = s.replace(item, '')
+
+                # Replace "Chapter A" with "Appendix A".
+                s = re.sub(r"Chapter ([A-Z])", r"Appendix \1", s)
+                f.write(s)
+
     os.chdir(cwd)
 
 @task
